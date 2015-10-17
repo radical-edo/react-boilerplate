2bedec2267e946d2abd736aa77fdf9fc56e3c6ba
radical-edo <radical.3do@gmail.com>
2015-10-17 06:05:28 +0200

    build task setup
    task build will automatically tasks necessary for the build process

--- gulp/index.js
+++ gulp/index.js
@@ -4,12 +4,19 @@ var path = require('path');
 var fs = require('fs');
 
 var gulp = require('gulp');
+var _ = require('lodash');
+
 var scripts = require('./utils/scripts');
 
 var tasks = fs.readdirSync(path.resolve(__dirname, 'tasks')).filter(scripts.filter).map(scripts.name);
 
 module.exports = function () {
   tasks.forEach(function (task) {
-    gulp.task(task, require(path.resolve(__dirname, 'tasks', task)));
+    var fn = require(path.resolve(__dirname, 'tasks', task));
+    if ('build' == task) {
+      gulp.task(task, _.without(tasks, 'build', 'watch'), fn)
+    } else {
+      gulp.task(task, fn);
+    }
   });
 };
--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -11,18 +11,6 @@ var bowerFiles = require('main-bower-files');
 // scss dependencies
 var sass = require('gulp-sass');
 
-function moveAssets() {
-  return gulp.src(config.paths.assets)
-    .pipe(gulp.dest(config.paths.dest));
-}
-
-function moveBowerFonts() {
-  return gulp.src(['bootstrap', 'font-awesome'].map(function (framework) {
-    return 'node_modules/' + framework + '/fonts/*.*';
-  }))
-  .pipe(gulp.dest(config.paths.dest + '/fonts'));
-}
-
 function buildStyles() {
   return gulp.src(config.paths.scss)
     .pipe(sass().on('error', sass.logError))
@@ -38,9 +26,6 @@ function buildVendorStyles() {
 }
 
 module.exports = function() {
-  moveAssets();
-  moveBowerFonts();
-  gulp.start('bundle');
   return gulp.src(config.paths.index)
     .pipe(inject(buildVendorStyles(), _.merge({ name: 'vendor' }, config.inject)))
     .pipe(inject(buildStyles(), config.inject))
