d9d715a645684c6dce86258cb12e8c55d727b3f7
radical-edo <radical.3do@gmail.com>
2015-11-05 04:44:54 +0100

    styles task has been removed
    it is now part of bundle

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -14,6 +14,6 @@ function run(task) {
 module.exports = function () {
   gulp.start('build');
   watch(config.paths.watchApp, run('bundle'));
-  watch(config.paths.watchStyles, run('styles'));
+  watch(config.paths.watchStyles, run('bundle'));
   watch(config.paths.watchAssets, run('assets'));
 };
