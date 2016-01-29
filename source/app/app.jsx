113b60639325fa9b29bc1cbe973fffa40f2e5dbe
radical_edo <radical.3do@gmail.com>
2016-01-29 10:55:30 +0100

    adding router to the app

--- source/app/app.jsx
+++ source/app/app.jsx
@@ -1,3 +1,9 @@
 'use strict';
 
 import React from 'react';
+import { render } from 'react-dom';
+
+import Routes from './routes';
+
+render(Routes, document.getElementById('app'));
+
---
+++ source/app/components/index.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+const Index = React.createClass({
+  render() {
+    return (
+      <p>May the force be with you!</p>
+    );
+  }
+});
+
+export default Index;
